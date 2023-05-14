6a89d89acd09e507f84737a6037c6d540a8a797e
cmarban <marbanchristian@gmail.com>
2023-05-14 19:54:43 -0400

    test2

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const { app, BrowserWindow } = require('electron');
-const path= require('path')
-const isDev=require('electron-is-dev')
+const { app, autoUpdater, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
 
-
-const updater = require('./updater');
+let mainWindow;
 
 const createWindow = () => {
-	const win = new BrowserWindow({
+	mainWindow = new BrowserWindow({
 		titleBarStyle: 'hidden',
 		titleBarOverlay: {
 			color: '#ffffff',
@@ -16,21 +15,47 @@ const createWindow = () => {
 		width: 800,
 		height: 600,
 	});
-	if(isDev){
-		console.log('corriendo en desarrollo')
-	}else{
-		console.log('produccion')
-		require('update-electron-app')()
+	if (isDev) {
+		console.log('corriendo en desarrollo');
+	} else {
+		console.log('produccion');
+		autoUpdater.checkForUpdates();
+		require('update-electron-app')();
 	}
 
-	win.loadFile(path.join(__dirname,'/front/index.html'));
+	mainWindow.loadFile(path.join(__dirname, '/front/index.html'));
 };
 
 app.whenReady().then(() => {
 	createWindow();
 });
 
-
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') app.quit();
+});
+
+autoUpdater.setFeedURL({
+	url: `https://github.com/onilinkman/tasty_desktop/releases/tag/v1.0.1`,
+	serverType: 'GitHub',
+});
+
+autoUpdater.on('update-available', () => {
+	console.log('Actualización disponible');
+});
+
+autoUpdater.on('update-not-available', () => {
+	console.log('No hay actualizaciones disponibles');
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+	console.log(`Progreso de descarga: ${progressObj.percent}%`);
+});
+
+autoUpdater.on('update-downloaded', () => {
+	console.log('Actualización descargada');
+	autoUpdater.quitAndInstall();
+});
+
+autoUpdater.on('error', (err) => {
+	console.error('Error durante la verificación de actualizaciones', err);
 });
