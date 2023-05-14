81ef46f92290453ab5af482c48500f10b14fd9c1
cmarban <marbanchristian@gmail.com>
2023-05-14 12:59:30 -0400

    created animations and new pages

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
 	const win = new BrowserWindow({
+		titleBarStyle: 'hidden',
+		titleBarOverlay: {
+			color: '#ffffff',
+			symbolColor: '#004aad',
+			height: 40,
+		},
 		width: 800,
 		height: 600,
 	});
 
-	win.loadFile("./front/index.html");
+	win.setOverlayIcon('./animal_cow.ico', 'Tasty');
+
+	win.loadFile('./front/index.html');
 };
 
 app.whenReady().then(() => {
@@ -14,5 +22,5 @@ app.whenReady().then(() => {
 });
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
+	if (process.platform !== 'darwin') app.quit();
+});
